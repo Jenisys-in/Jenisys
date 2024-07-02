801689a58a955d7eb440572077cfd296a5c57c89
Tuhin Das <tuhin@Tuhins-MacBook-Air.local>
2024-07-03 02:44:32 +0530

    changes in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
