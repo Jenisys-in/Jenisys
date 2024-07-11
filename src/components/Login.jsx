bd5850de8437e805c8fe55a2396224ba24bcee24
Hridhwesh Maity <hridhweshrc2014@gmail.com>
2024-07-12 02:01:54 +0530

    login page done 99%

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,12 +16,46 @@ const LogIn = () => {
       />
       </div>
       
-       <div className="mt-[50px] relative align-items-center bg-[linear-gradient(180deg,#FFFDF6_0%,#1E12A7_100%)]">
-       <h1 className="font-['Encode-Sans-Semi-Expanded']">Welcome</h1>
-          <h1>Don't have an account? </h1>
-          <button className="active:border-white duration-300 active:text-white absolute ">
-            Learn More
-          </button>
+       <div className="mt-[80px] relative align-items-center bg-[linear-gradient(180deg,#FFFDF6_0%,#1E12A7_100%)]  ">
+        <div className="mt-[40px]">
+       <h1 className="font-['Encode_Sans_Semi_Expanded'] text-[70px] px-[150px] self-stretch text-white">Welcome</h1>
+       <div className="flex flex-row relative">
+          <h1 className="font-['Encode_Sans_Semi_Expanded'] px-[150px] text-[20px] pt-[35px] text-white">Don't have an account? </h1>
+          <button className=" text-[18px] active:border-white duration-300 active:text-white absolute left-[390px] top-[30px] bg-white text-black border-zinc-200 rounded-full px-4 py-2 drop-shadow-xl">
+            Sign Up
+          </button> 
+          </div>
+          </div>
+          <div className="mt-[60px]">
+            <h1 className="pl-[90px] text-white text-[20px]"
+            >Username</h1>
+            <input
+          type="usn"
+          className=" mt-[10px] ml-[70px] text-black font-semibold pr-[170px] py-[20px] rounded-full white drop-shadow-xl"
+          />
+          <h1 className="pl-[90px] text-white  text-[20px] mt-[10px]"
+            >Password</h1>
+            <input
+          type="usn"
+          className=" mt-[10px] ml-[70px] text-black font-semibold pr-[170px] py-[20px] rounded-full white drop-shadow-xl"
+          />
+          </div>
+          <div className="flex flex-row pl-[80px] mt-[20px]">
+            <h3 className="text-[20px] text-white">
+          <input type="checkbox" /> Keep me logged in
+          </h3>
+          <h3
+          className="text-white text-[20px] pl-[100px] underline"> Forgot Password?</h3>
+
+          </div>
+          <button className=" text-[18px] active:border-white duration-300 active:text-white ml-[270px] left-[390px] top-[30px] bg-white text-black border-zinc-200 rounded-full px-4 py-2 drop-shadow-xl mt-[20px]">
+            Log Up
+           
+          </button> 
+          <button className=" text-[18px] active:border-white duration-300 active:text-white ml-[110px] left-[390px]  bg-white text-black border-zinc-200 rounded-full px-32 py-2 drop-shadow-xl mt-[40px] mb-[55px]">
+            Log in with Google
+          </button> 
+
         </div>        
    
       </div>
