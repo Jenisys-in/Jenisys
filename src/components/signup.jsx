24df3a2e4cff51685c2332f9e15e1c24b18e6086
Hridhwesh Maity <hridhweshrc2014@gmail.com>
2024-07-13 00:32:22 +0530

    sign up

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../App.css";
 import "@fontsource/lexend-exa";
 
-const LogIn = () => {
+const SignUp = () => {
   return (
     <div className="mt-[35px]  relative flex  items-start text-2xl w-fit max-md:max-w-full min-h-screen">
       <div>
@@ -21,48 +21,59 @@ const LogIn = () => {
           </h1>
           <div className="flex flex-row relative">
             <h1 className="font-['Encode_Sans_Semi_Expanded'] px-[150px] text-[20px] pt-[35px] text-white ">
-              Don't have an account?{" "}
+              Already have an account?{" "}
             </h1>
-            <button className=" duration-300 hover:[box-shadow:0_7px_8px_0_#E6BE8A_,_0_7px_50px_0_#E6BE8A] hover:bg-[#F9A825] text-[18px] active:border-white duration-300 active:text-white absolute left-[390px] top-[30px] bg-white text-black border-zinc-200 rounded-full px-4 py-2 drop-shadow-xl">
-              Sign Up
+            <button className=" duration-300 hover:[box-shadow:0_7px_8px_0_#E6BE8A_,_0_7px_50px_0_#E6BE8A] hover:bg-[#F9A825] text-[18px] active:border-white duration-300 active:text-white absolute left-[410px] top-[30px] bg-white text-black border-zinc-200 rounded-full px-4 py-2 drop-shadow-xl">
+              Sign In
             </button>
           </div>
         </div>
         <form>
         <div className="mt-[60px]">
-          <h1 className="pl-[90px] text-white text-[20px]">Username</h1>
+          <h1 className="pl-[90px] text-white text-[20px] mt-[10px]">Name</h1>
+          <input
+            id="name"
+            className=" mt-[10px] ml-[70px] text-black font-semibold pr-[170px] py-[20px] rounded-full white drop-shadow-xl"
+          />
+          <h1 className="pl-[90px] text-white text-[20px] mt-[10px]">Username</h1>
           <input
             id="usn"
             className=" mt-[10px] ml-[70px] text-black font-semibold pr-[170px] py-[20px] rounded-full white drop-shadow-xl"
           />
+          <h1 className="pl-[90px] text-white text-[20px] mt-[10px]">Password</h1>
+          <input
+            id="pass1"
+            type="password"
+            className=" mt-[10px] ml-[70px] text-black font-semibold pr-[170px] py-[20px] rounded-full white drop-shadow-xl"
+          />
+          
           <h1 className="pl-[90px] text-white  text-[20px] mt-[10px]">
-            Password
+            Re-enter Password
           </h1>
           <input
-            id="pass"
+            id="pass2"
             type="password"
             className=" mt-[10px] ml-[70px] text-black font-semibold pr-[170px] py-[20px] rounded-full white drop-shadow-xl"
           />
         </div>
-        <div className="flex flex-row pl-[80px] mt-[20px]">
-          <h3 className="text-[20px] text-white">
-            <input type="checkbox" /> Keep me logged in
-          </h3>
-          <h3 className="text-white text-[20px] pl-[100px] underline">
-            {" "}
-            Forgot Password?
-          </h3>
-        </div>
         <button className=" text-[18px] hover:[box-shadow:0_7px_8px_0_#E6BE8A_,_0_7px_50px_0_#E6BE8A] hover:bg-[#F9A825] active:border-white duration-300 active:text-white ml-[270px] left-[390px] top-[30px] bg-white text-black border-zinc-200 rounded-full px-4 py-2 drop-shadow-xl mt-[20px]">
-          Log in
+          Sign Up
+
         </button>
+        <div className="flex flex-row relative">
+        <img 
+        className="w-[40px] h-[40px] absolute left-[200px] top-[55px]"
+        src="https://yt3.googleusercontent.com/viNp17XpEF-AwWwOZSj_TvgobO1CGmUUgcTtQoAG40YaYctYMoUqaRup0rTxxxfQvWw3MvhXesw=s900-c-k-c0x00ffffff-no-rj"
+        />
+        
+        </div>
         </form>
         <button className=" text-[18px]  hover:[box-shadow:0_7px_8px_0_#E6BE8A_,_0_7px_50px_0_#E6BE8A] hover:bg-[#F9A825] active:border-white duration-300 active:text-white ml-[110px] left-[390px]  bg-white text-black border-zinc-200 rounded-full px-32 py-2 drop-shadow-xl mt-[40px] mb-[55px]">
-          Log in with Google
+            Sign Up with Google
         </button>
       </div>
     </div>
   );
 };
 
-export default LogIn;
+export default SignUp;
