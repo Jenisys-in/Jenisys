5a3b020677c170d4682b67ceef0462e09996723a
Tuhin Das <tuhin@Tuhins-MacBook-Air.local>
2024-08-03 02:36:34 +0530

    contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,69 +1,114 @@
-import React from 'react';
+import React from "react";
 import "../App.css";
 
-function Contact(){
-    return(
-            <div className="mt-[90px] pt-[10px] self-stretch bg-radial-gradient flex flex-row ">
-                <div className="pt-[20px] text-white font-['Inter'] pl-[40px] text-[20px] ml-[20px] flex flex-col">
-                    <h1 className='mb-[7px]'>Name</h1>
-                    <input
-                    type="text"
-                    className='pl-[10px] rounded-full w-[500px] h-[45px] text-black focus:shadow-black focus:shadow-lg'></input>
-                    <h1 className='mb-[7px] mt-[20px]'>E-Mail</h1>
-                    <input
-                    type="text"
-                    className='pl-[10px] rounded-full w-[500px] h-[45px] text-black focus:shadow-black focus:shadow-lg'></input>
-                    <h1  className='mb-[7px] mt-[20px]'>Phone Number</h1>
-                    <input
-                    type="text"
-                    className='pl-[10px] rounded-full w-[500px] h-[45px] text-black focus:shadow-black focus:shadow-lg'></input>
-                    <h1  className='mb-[10px] mt-[20px]'>Message</h1>
-                    <textarea
-                    className='pl-[10px] rounded-lg w-[500px] h-[200px] text-black focus:shadow-black focus:shadow-lg'></textarea>
-                    <button className="rounded-full w-[120px] py-2 bg-[#5851AD] mt-[25px] mb-[41px] shadow-lg shadow-black hover:bg-[#FFFFFF] hover:text-[#5851AD] hover:shadow-white">
-                        Submit
-                    </button>
-
-                </div>
-                <div className="pt-[20px] pl-[210px] mt-[40px] font-['Inter'] relative text-white flex flex-col pr-[70px]">
-                    <h1 className='text-[50px]'>Contact Us</h1>
-                    <h1 
-                    className='text-[24px] leading-6 mt-[10px]'
-                    >Over the last several weeks, we’ve had Google I/O, which highlighted the rollout of Google’s Gemini AI engine for smartphones; Microsoft Build, which focused on its Copilot+ rollout for PCs; and Apple’s WWDC24 video, which showcased the rollout of AI on both PCs and smartphones. Let’s explor...</h1>
-                    <img 
-                    src="img/Phone call.png  "
-                    className='w-[60px] h-[60px] bg-[#918ADF] p-4 rounded-full mt-[20px]'
-                    />
-                    <h1 className='absolute top-[305px] right-[420px] text-[20px]'>+365454321</h1>
-                    <img 
-                    src="img/Mail.png"
-                    className='w-[60px] h-[60px] bg-[#918ADF] p-4 rounded-full mt-[20px]'
-                    />
-                    <h1 className='absolute top-[390px] right-[380px] text-[20px]'>abcd@gmail.com</h1>
-                    <img 
-                    src="img/Map pin.png"
-                    className='w-[60px] h-[60px] bg-[#918ADF] p-4 rounded-full mt-[20px]' 
-                    />
-                    <h1 className='absolute top-[465px] right-[411px] text-[20px]'>Kolkata, India</h1>
-                    <img
-                    src="/img/ellipse.jpeg"
-                    alt="Logo"
-                    className="ml-[500px] -mt-[20px] w-10 h-10 rounded-full"
-                    />
-                    <img
-                    src="/img/ellipse.jpeg"
-                    alt="Logo"
-                    className="ml-[400px] mt-[30px] w-16 h-16 rounded-full"
-                    />
-
-                </div>
-
+function Contact() {
+  return (
+    <div className="flex overflow-hidden flex-col items-center px-20 pb-56 bg-white max-md:px-5 max-md:pb-24">
+      <div className="mt-28 text-5xl font-bold tracking-tighter leading-tight text-black max-md:mt-10 max-md:text-4xl">
+        Contact Us
+      </div>
+      <div className="mt-11 text-lg font-semibold tracking-tight leading-8 text-center text-black w-[915px] max-md:mt-10 max-md:max-w-full">
+        If you have any questions about our services, want to discuss a
+        potential collaboration, or just need advice on your tech strategy, our
+        team is here to help. Get in touch with us today.
+      </div>
+      <div className="mt-20 text-4xl font-bold tracking-tighter leading-tight text-black max-md:mt-10 max-md:max-w-full">
+        Contact our team for a consultation
+      </div>
+      <div className="mt-20 w-full max-w-[1147px] max-md:mt-10 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col">
+          <div className="flex flex-col w-[41%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow items-center leading-tight text-black max-md:mt-10">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/c259cefb2f8833ec7b6a447f60406cf5d2903ef4a0874fdc696704a888fb5287?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/c259cefb2f8833ec7b6a447f60406cf5d2903ef4a0874fdc696704a888fb5287?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/c259cefb2f8833ec7b6a447f60406cf5d2903ef4a0874fdc696704a888fb5287?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/c259cefb2f8833ec7b6a447f60406cf5d2903ef4a0874fdc696704a888fb5287?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/c259cefb2f8833ec7b6a447f60406cf5d2903ef4a0874fdc696704a888fb5287?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/c259cefb2f8833ec7b6a447f60406cf5d2903ef4a0874fdc696704a888fb5287?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/c259cefb2f8833ec7b6a447f60406cf5d2903ef4a0874fdc696704a888fb5287?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/c259cefb2f8833ec7b6a447f60406cf5d2903ef4a0874fdc696704a888fb5287?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392"
+                className="object-contain max-w-full rounded-lg aspect-square w-[105px]"
+              />
+              <div className="mt-7 text-3xl font-semibold tracking-tight">
+                Talk to our Team
+              </div>
+              <div className="self-stretch mt-6 text-xl tracking-tight">
+                +14047365291, +918240384648
+              </div>
             </div>
-
-
-    );
-
-
+          </div>
+          <div className="flex flex-col ml-5 w-[27%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow items-center leading-tight text-black max-md:mt-10">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/b9e360ac99f97e7e9c7fbc97a2002af4ee2f1252bd083a868dc859b84d53d3a3?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/b9e360ac99f97e7e9c7fbc97a2002af4ee2f1252bd083a868dc859b84d53d3a3?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/b9e360ac99f97e7e9c7fbc97a2002af4ee2f1252bd083a868dc859b84d53d3a3?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/b9e360ac99f97e7e9c7fbc97a2002af4ee2f1252bd083a868dc859b84d53d3a3?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/b9e360ac99f97e7e9c7fbc97a2002af4ee2f1252bd083a868dc859b84d53d3a3?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/b9e360ac99f97e7e9c7fbc97a2002af4ee2f1252bd083a868dc859b84d53d3a3?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/b9e360ac99f97e7e9c7fbc97a2002af4ee2f1252bd083a868dc859b84d53d3a3?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/b9e360ac99f97e7e9c7fbc97a2002af4ee2f1252bd083a868dc859b84d53d3a3?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392"
+                className="object-contain max-w-full rounded-lg aspect-square w-[105px]"
+              />
+              <div className="mt-7 text-3xl font-semibold tracking-tight">
+                Email Us
+              </div>
+              <div className="self-stretch mt-5 text-xl tracking-tight">
+                tuhindas@jenisys.in
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col ml-5 w-[32%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow items-center leading-tight text-black max-md:mt-10">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/953cbefbd3c0e319848d7047fb41a447c592c28c3c451256fe18472cb73f56d1?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/953cbefbd3c0e319848d7047fb41a447c592c28c3c451256fe18472cb73f56d1?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/953cbefbd3c0e319848d7047fb41a447c592c28c3c451256fe18472cb73f56d1?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/953cbefbd3c0e319848d7047fb41a447c592c28c3c451256fe18472cb73f56d1?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/953cbefbd3c0e319848d7047fb41a447c592c28c3c451256fe18472cb73f56d1?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/953cbefbd3c0e319848d7047fb41a447c592c28c3c451256fe18472cb73f56d1?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/953cbefbd3c0e319848d7047fb41a447c592c28c3c451256fe18472cb73f56d1?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/953cbefbd3c0e319848d7047fb41a447c592c28c3c451256fe18472cb73f56d1?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392"
+                className="object-contain max-w-full rounded-lg aspect-square w-[105px]"
+              />
+              <div className="self-stretch mt-7 text-3xl font-semibold tracking-tight">
+                Operating Hours
+              </div>
+              <div className="mt-4 text-xl tracking-tight">8am to 5pm</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-32 w-full max-w-[1325px] max-md:mt-10 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col">
+          <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col mt-28 w-full leading-tight text-black max-md:mt-10 max-md:max-w-full">
+              <div className="self-start text-5xl font-bold tracking-tighter max-md:text-4xl">
+                Let’s Talk Tech
+              </div>
+              <div className="flex flex-wrap gap-5 items-start mt-20 max-md:mt-10 max-md:mr-0.5 max-md:max-w-full">
+                <div className="flex flex-col flex-1 grow shrink-0 basis-0 w-fit">
+                  <div className="self-start text-2xl font-semibold tracking-tight">
+                    Name
+                  </div>
+                  <div className="px-3.5 py-5 mt-6 text-base font-light tracking-tight rounded-md shadow-sm max-md:pr-5">
+                    Ex. Parker
+                  </div>
+                </div>
+                <div className="flex flex-col flex-1 grow shrink-0 basis-0 w-fit">
+                  <div className="self-start text-2xl font-semibold tracking-tight">
+                    Email
+                  </div>
+                  <div className="px-3.5 py-4 mt-6 text-base font-light tracking-tight rounded-md shadow-sm max-md:pr-5">
+                    Ex. yourmail@gmail.com
+                  </div>
+                </div>
+              </div>
+              <div className="self-start mt-12 text-2xl font-semibold tracking-tight max-md:mt-10">
+                Message
+              </div>
+              <div className="px-3.5 py-5 mt-5 text-base font-light tracking-tight rounded-md shadow-sm max-md:pr-5 max-md:max-w-full">
+                Type Your Message Here
+              </div>
+              <div className="px-16 py-8 mt-14 text-2xl font-semibold tracking-tight text-white bg-indigo-800 rounded-md max-md:px-5 max-md:mt-10 max-md:mr-0.5 max-md:max-w-full">
+                Get Started
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/31b99f91a536bf5fd77dbe61227cd7e8d92011d52c400e342fe0421e08fda931?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/31b99f91a536bf5fd77dbe61227cd7e8d92011d52c400e342fe0421e08fda931?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/31b99f91a536bf5fd77dbe61227cd7e8d92011d52c400e342fe0421e08fda931?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/31b99f91a536bf5fd77dbe61227cd7e8d92011d52c400e342fe0421e08fda931?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/31b99f91a536bf5fd77dbe61227cd7e8d92011d52c400e342fe0421e08fda931?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/31b99f91a536bf5fd77dbe61227cd7e8d92011d52c400e342fe0421e08fda931?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/31b99f91a536bf5fd77dbe61227cd7e8d92011d52c400e342fe0421e08fda931?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/31b99f91a536bf5fd77dbe61227cd7e8d92011d52c400e342fe0421e08fda931?apiKey=150ca4726f0b413090f132e093d2a392&&apiKey=150ca4726f0b413090f132e093d2a392"
+              className="object-contain grow w-full rounded-md aspect-[1.03] max-md:mt-10 max-md:max-w-full"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
