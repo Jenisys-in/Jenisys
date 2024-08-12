8d2971926c722673050c93330076219c0c91de55
Tuhin Das <tuhin@Tuhins-MacBook-Air.local>
2024-08-12 17:15:56 +0530

    preloader and transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/navbar.jsx";
 import Home from "./components/Homepage.jsx";
 import LogIn from "./components/Login.jsx";
@@ -9,24 +9,52 @@ import Blog from "./components/Blog.jsx";
 import SignUp from "./components/signup.jsx";
 import NewBlog from "./components/new-blog.jsx";
 import Contact from "./components/contact.jsx";
-
+import Preloader from "./components/preloader.jsx";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isPreloaderVisible, setIsPreloaderVisible] = useState(true);
+
+  useEffect(() => {
+    // Set a timer to hide the preloader after 5 seconds
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000); // 5000 milliseconds = 5 seconds
+
+    // Cleanup the timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      // Hide the preloader with a slight delay to allow the fade-out transition
+      const timeout = setTimeout(() => setIsPreloaderVisible(false), 1000); // 1000 milliseconds = 1 second
+      return () => clearTimeout(timeout);
+    }
+  }, [isLoading]);
+
   return (
-    <Router>
-      <ScrollToTop />
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/Blog" element={<Blog />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/newblog" element={<NewBlog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<LogIn />} />
-      </Routes>
-    </Router>
+    <>
+      {isPreloaderVisible && <Preloader isVisible={isLoading} />}
+      <div
+        className={`transition-opacity duration-1000 ${
+          isPreloaderVisible ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        <Router>
+          <ScrollToTop />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Blog" element={<Blog />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/newblog" element={<NewBlog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<LogIn />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/preloader.jsx
@@ -0,0 +1,20 @@
+// Preloader.jsx
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center bg-black z-50">
+      <div className="w-1/2 h-auto ">
+        <video className="w-full h-full object-cover" autoPlay muted loop>
+          <source
+            src="/Colorful Modern Infinity Technology Free Logo-4.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
