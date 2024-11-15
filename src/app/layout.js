69727be8d8da8b43c0863d9a10e3342f88b67a18
Tuhin Das <55959064+09TuhinDas@users.noreply.github.com>
2024-11-16 03:42:33 +0530

    fff

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 "use client";
 
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import ScrollToTop from '@/components/ScrolltoTop';
