f2bd0c32289290a55e8b7a0778e60862eac917b3
Tuhin Das <55959064+09TuhinDas@users.noreply.github.com>
2024-11-16 03:44:48 +0530

    fffff

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,6 +31,8 @@ export default function RootLayout({ children }) {
           {children}
           <ScrollToTop />
         </div>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
