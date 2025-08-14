fea4870e7fe949ca1bd0f30ca129f3bdf75f7cf6
Tuhin Das <09tuhindas@gmail.com>
2025-08-14 04:50:56 -0400

    new

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,3 @@
-const { blogs } = require("./src/lib/blogData");
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.jenisys.in",
@@ -7,41 +5,6 @@ module.exports = {
   sitemapSize: 7000,
   changefreq: "daily",
   priority: 0.7,
-  transform: async (config, path) => {
-    const blogPostRegex = /^\/blog\/(.+)$/;
-    const servicePageRegex = /^\/services\/(.+)$/;
-    const blogMatch = path.match(blogPostRegex);
-    const serviceMatch = path.match(servicePageRegex);
-
-    if (blogMatch) {
-      const slug = blogMatch[1];
-      const blog = blogs.find((b) => b.slug === slug);
-      if (blog) {
-        return {
-          loc: path,
-          changefreq: "weekly",
-          priority: 0.8,
-          lastmod: blog.dateModified || new Date().toISOString(),
-        };
-      }
-    }
-
-    if (serviceMatch) {
-      return {
-        loc: path,
-        changefreq: "monthly",
-        priority: 0.9,
-        lastmod: new Date().toISOString(),
-      };
-    }
-
-    return {
-      loc: path,
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: new Date().toISOString(),
-    };
-  },
   robotsTxtOptions: {
     policies: [
       {
