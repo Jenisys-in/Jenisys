d05dad2df9aac16e8bfdd5f0471393fec063df3b
Tuhin Das <09tuhindas@gmail.com>
2025-09-10 19:43:38 +0530

    new

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,8 +1,3 @@
 export default function SchedulePage() {
-  return (
-    <main style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Jenisys Scheduler</h1>
-      <p>This is the scheduling interface.</p>
-    </main>
-  );
+  return <div>Scheduler test</div>;
 }
