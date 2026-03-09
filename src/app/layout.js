f83c220a9f83f938bac30cfa442e3d242c0f0031
Tuhin Das <09tuhindas@gmail.com>
2026-03-09 18:17:30 +0530

    apollo integrated

--- src/app/layout.js
+++ src/app/layout.js
@@ -169,8 +169,19 @@ export default function RootLayout({ children }) {
           `}
         </Script>
 
+        {/* Apollo Integration */}
+        <Script id="apollo-tracker" strategy="afterInteractive">
+          {`
+            function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+            o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+            o.onload=function(){window.trackingFunctions.onLoad({appId:"69aebe8fb50a1b0021786848"})},
+            document.head.appendChild(o)}initApollo();
+          `}
+        </Script>
+
       </body>
     </html>
+
   );
 }
 
